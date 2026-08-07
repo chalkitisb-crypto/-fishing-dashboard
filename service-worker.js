6ba5e7730c38c436309db79fd6b5062c1ee35444
chalkitisb-crypto <chalkitisb@gmail.com>
2026-08-07 19:05:58 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "fishing-dashboard-v13.0.0";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./moon_sphere.png",
+  "./fish_left.png",
+  "./fish_right.png",
+  "./fish_only.png",
+  "./sun_scene.png",
+  "./activity_tsipoura.jpg",
+  "./score_gauge.jpg",
+  "./tech_card_spinning.png",
+  "./tech_card_lrf.png",
+  "./tech_card_english.png",
+  "./tech_card_shore_jig.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        }).catch(() => cached)
+      );
+    })
+  );
+});
